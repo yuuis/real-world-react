429c014b3d4737d2161fd15efa3c63450a6c6cce
Yu Ishiguro <yuuis.egr@gmail.com>
2019-06-24 06:29:00 +0900

    add Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,71 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const LoggedOutView = props => {
+  if (!props.currentUser) {
+    return (
+      <ul className="nav navbar-nav pull-xs-right">
+
+        <li className="nav-item">
+          <Link to="/" className="nav-link">
+            home
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link to="/login" className="nav-link">
+            sign in
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link to="/register" className="nav-link">
+            sign up
+          </Link>
+        </li>
+
+      </ul>
+    )
+  }
+  return null;
+};
+
+const LoggedInView = props => {
+  if (props.currentUser) {
+    return (
+      <ul className="nav navbar-nav pull-xs-right">
+
+        <li className="nav-item">
+          <Link to="/" className="nav-link">
+            home
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link to="/editor" className="nav-link">
+            <i className="icon-compose"></i>&nbsp;new post
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link to="/settings" className="nav-link">
+            <i className="icon-gear-a"></i>&nbsp;settings
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link to={`/@${props.currentUser.username}`} className="nav-link">
+            <img src={props.currentUser.image} className="user-pic" alt={props.currentUser.username}/>
+            {props.currentUser.username}
+          </Link>
+        </li>
+
+      </ul>
+    );
+  }
+  return null;
+};
+
 class Header extends React.Component {
   render() {
     return (
@@ -10,6 +75,9 @@ class Header extends React.Component {
           <Link to="/" className="navbar-brand">
             {this.props.appName.toLowerCase()}
           </Link>
+
+          <LoggedOutView currentUser={this.props.currentUser} />
+          <LoggedInView currentUser={this.props.currentUser} />
 
         </div>
       </nav>
