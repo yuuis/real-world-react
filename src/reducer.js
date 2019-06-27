5f6c6fd8fda4f325789f7f7a39d34b6f79a74a33
Yu Ishiguro <yuuis.egr@gmail.com>
2019-06-27 19:08:26 +0900

    add reducer for profile

--- src/reducer.js
+++ src/reducer.js
@@ -3,10 +3,12 @@ import { routerReducer } from 'react-router-redux';
 import auth from './reducers/auth';
 import common from './reducers/common';
 import settings from './reducers/settings';
+import profile from './reducers/profile'
 
 export default combineReducers({
   auth,
   common,
   settings,
+  profile,
   router: routerReducer
 });
